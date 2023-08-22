8ad5adddafde8ecc41d2126c66fc42709fb1d57f
mayanksdudakiya <mayanksdudakiya@gmail.com>
2023-08-22 12:50:12 +0530

    feat: Transition class added

---
+++ src/Transitions.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Mayanksdudakiya\StateMachine;
+
+class Transitions
+{
+    public function __construct(
+        public string $name = '',
+        public string $initialStateName = '',
+        public string $resultingStateName = '',
+        public string $acceptConditionCallable = '',
+        public array $metadata = [],
+    ) {
+    }
+}
